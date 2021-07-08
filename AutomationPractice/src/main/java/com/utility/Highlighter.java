c56131d72cfbd39cf7daf6ece89e1acb99dcb011
fatem <fatem@192.168.1.12>
2021-07-07 22:53:01 -0400

    Home Work

---
+++ AutomationPractice/src/main/java/com/utility/Highlighter.java
@@ -0,0 +1,18 @@
+package com.utility;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+public class Highlighter {
+
+	public static void addColor(WebDriver driver, WebElement signinbtn) {
+		 
+		
+	}
+
+	public static void addColor1(WebDriver driver, WebElement signinbtn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ AutomationPractice/src/test/java/com/smoke/PraciceString.java
@@ -0,0 +1,104 @@
+package com.smoke;
+
+import java.util.Arrays;
+
+public class PraciceString {
+
+
+ static String countryName = "United States of America";	// GLOBAL >> INSTANCE
+	
+	 static void getCountryname() {
+	
+	
+		
+	//for (int a=0;a<=100;a++) {
+		
+	//System.out.println(a);	
+		
+	//}
+		
+		
+		
+		
+	
+String[] pieces = countryName.split(" ");	
+	
+System.out.println(Arrays.toString(pieces));
+
+
+
+	
+}
+
+public static void main(String[] args) {
+	getCountryname();
+	
+	// PraciceString obj = new PraciceString();
+	//obj.getPieces();
+	
+	
+	
+	//new PraciceString().getPieces();
+	
+	
+	
+	 
+	
+	
+	
+}	
+	
+	
+	
+	
+
+	
+	
+	
+	
+	
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//static int a = 10;
+	//static int b = 20;
+	
+	
+	//public static void main(String[] args) {
+			
+		
+//System.out.println("a value = "+a);	//20
+	//System.out.println("b value = "+b);	//10
+		
+		
+	//a=a+b;//30
+	//b=a-b;//10	
+	//a=a-b;//20 
+	
+	
+	//System.out.println("a value = "+a);//10
+	//System.out.println("b value ="+b);//20
