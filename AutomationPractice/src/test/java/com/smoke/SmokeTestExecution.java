7d78a5b6d2951467dbb29d12e3c6def26baa98e4
fatem <fatem@192.168.1.12>
2021-07-07 22:58:09 -0400

    H.W

---
+++ AutomationPractice/src/test/java/com/smoke/SmokeTestExecution.java
@@ -0,0 +1,22 @@
+package com.smoke;
+
+import com.generic.AutomationPracticeLogin;
+
+public class SmokeTestExecution {
+
+	
+ public static void main(String[] args) {
+	 
+	 
+	 AutomationPracticeLogin obj= new AutomationPracticeLogin();
+	 
+	 obj.baselogin();
+	 
+	 
+	 
+	 
+	
+}	
+	
+	
+}
