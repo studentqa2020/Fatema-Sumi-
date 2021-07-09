b9af1fcc81fe81b3d06c82a6902cacc74b2c9b6a
fatem <fatem@192.168.1.10>
2021-07-08 23:45:25 -0400

    TestNG Basic Practice

--- AutomationPractice/src/test/java/com/smoke/SmokeTestExecution.java
+++ AutomationPractice/src/test/java/com/smoke/SmokeTestExecution.java
@@ -1,22 +1,40 @@
 package com.smoke;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
 import com.generic.AutomationPracticeLogin;
 
 public class SmokeTestExecution {
 
 	
- public static void main(String[] args) {
+ @BeforeTest
 	 
+	public void setup() {
+	WebDriver driver = new ChromeDriver();
 	 
-	 AutomationPracticeLogin obj= new AutomationPracticeLogin();
-	 
-	 obj.baselogin();
-	 
-	 
+ } 
+@Test
+public void loginTest() {
+	new AutomationPracticeLogin().baselogin();
+
+	
+}
+@AfterTest
+public void teardown() {
+	
+ teardown();
+	
+	
+	
+}
 	 
 	 
 	
-}	
+
 	
 	
 }
